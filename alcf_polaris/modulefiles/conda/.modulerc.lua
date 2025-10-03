dbb6c13666de55f5eab75a608c802f224dc09f3f
Kyle Gerard Felker <felker@anl.gov>
2025-10-03 19:36:19 +0000

    comment out future change

--- alcf_polaris/modulefiles/conda/.modulerc.lua
+++ alcf_polaris/modulefiles/conda/.modulerc.lua
@@ -1,2 +1,3 @@
-module_version("conda/2025-09-24", "default")
+-- module_version("conda/2025-09-24", "default")
+module_version("conda/2024-04-29", "default")
 hide_modulefile("2025-09-24-aws-nccl-1.9.1")
