15ee07d742b1c11afa0fff2c7a69c7751e6f5532
Kyle Gerard Felker <felker@anl.gov>
2025-10-08 06:04:51 +0000

    preparing to add new fresh build with pinned vLLM, verl, transformerengine

--- alcf_polaris/modulefiles/conda/.modulerc.lua
+++ alcf_polaris/modulefiles/conda/.modulerc.lua
@@ -1,3 +1,5 @@
 -- module_version("conda/2025-09-25", "default")
+-- module_version("conda/2025-09-26", "default")
 module_version("conda/2024-04-29", "default")
 hide_modulefile("2025-09-25-aws-nccl-1.9.1")
+hide_modulefile("2025-09-26-aws-nccl-1.9.1")
